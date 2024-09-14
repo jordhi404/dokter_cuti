bc7b5d4684bb3442018334b297e887715259da0e
jordhi404 <tejosaputro22@gmail.com>
2024-09-14 10:04:19 +0700

    Init commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DoctorStatusController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DoctorStatusController::class, 'index'])->name('index');
