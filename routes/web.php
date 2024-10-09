7933394cf6a10717c6bf3d5f46ee315071f0c893
jordhi404 <tejosaputro22@gmail.com>
2024-10-09 14:26:56 +0700

    Perubahan route

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RegulerDoctorStatusController;
-use App\Http\Controllers\Non_regulerDoctorStatusController;
-use App\Http\Controllers\theDoctorStatusController;
 use App\Http\Controllers\offDatesController;
 
-Route::get('/reguler', [RegulerDoctorStatusController::class, 'regulerIndex'])->name('regulerIndex');
-Route::get('/non-reguler', [Non_regulerDoctorStatusController::class, 'nonRegulerIndex'])->name('nonRegulerIndex');
-
-Route::get('/', [theDoctorStatusController::class, 'index'])->name('index');
-
-Route::get('/offdates', [offDatesController::class, 'processDoctorOffDates'])->name('offDatesIndex');
+Route::get('/', [offDatesController::class, 'processDoctorOffDates'])->name('offDatesIndex');
