ba4405b387c77fc736d27d973b6429ab3ad2c0ba
jordhi404 <tejosaputro22@gmail.com>
2024-12-28 08:41:48 +0700

    Penambahan kondisi untuk badge dokter

--- public/script.js
+++ public/script.js
@@ -76,11 +76,13 @@ function createSliderHtml(doctorData, leaveType) {
                       ((new Date(cuti.cuti_start).setHours(0, 0, 0, 0) === new Date(cuti.cuti_end).setHours(0, 0, 0, 0)) && new Date(cuti.cuti_start).setHours(0, 0, 0, 0) === new Date().setHours(0, 0, 0, 0))
                       ? 'Tidak Praktik Hari Ini'
                       :(new Date(cuti.cuti_start).setHours(0, 0, 0, 0) === new Date(cuti.cuti_end).setHours(0, 0, 0, 0))
-                      ? 'Tidak Praktik pada tgl ' + new Date(cuti.cuti_start).toLocaleDateString('id-ID', { day: 'numeric', month: 'long'})
+                      ? 'Tidak Praktik tgl ' + new Date(cuti.cuti_start).toLocaleDateString('id-ID', { day: 'numeric', month: 'long'})
                       :(new Date(cuti.cuti_start).setHours(0, 0, 0, 0) <= new Date().setHours(0, 0, 0, 0) && new Date(cuti.cuti_end).setHours(0, 0, 0, 0) >= new Date().setHours(0, 0, 0, 0))
                       ? 'Tidak Praktik s.d. ' + new Date(cuti.cuti_end).toLocaleDateString('id-ID', { day: 'numeric', month: 'long'})
                       : (new Date(cuti.cuti_start).setHours(0, 0, 0, 0) > new Date().setHours(0, 0, 0, 0) && new Date(cuti.cuti_end).setHours(0, 0, 0, 0) > new Date().setHours(0, 0, 0, 0))
-                      ? 'Tidak Praktik pada tgl ' + new Date(cuti.cuti_start).toLocaleDateString('id-ID', { day: 'numeric', month: 'long'}) + ' s.d. ' + new Date(cuti.cuti_end).toLocaleDateString('id-ID', { day: 'numeric', month: 'long'})
+                        ? (new Date(cuti.cuti_start).getMonth() === new Date(cuti.cuti_end).getMonth())
+                          ? 'Tidak Praktik tgl ' + new Date(cuti.cuti_start).toLocaleDateString('id-ID', { day: 'numeric'}) + ' s.d. ' + new Date(cuti.cuti_end).toLocaleDateString('id-ID', { day: 'numeric', month: 'long'})
+                          : 'Tidak Praktik tgl ' + new Date(cuti.cuti_start).toLocaleDateString('id-ID', { day: 'numeric', month: 'long'}) + ' s.d. ' + new Date(cuti.cuti_end).toLocaleDateString('id-ID', { day: 'numeric', month: 'long'})
                       : 'Tidak Praktik'
                     }
                   </strong>
